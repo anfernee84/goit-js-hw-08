a3062eb446b1818ca6e15c2e0562891b125d04ac
anfernee84 <eduard.rogulin@outlook.com>
2022-08-31 14:47:25 +0300

    add_task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+///////////////////////////////////////////////////////////create gallery/////////////////
+const rootGallery = document.querySelector('.gallery');
+const imageGallery = createGallery(galleryItems);
+rootGallery.insertAdjacentHTML('afterbegin', imageGallery);
+
+function createGallery(arrOfObj) {
+  return arrOfObj
+    .map(elem => {
+      const { preview, original, description } = elem;
+      return `<a 
+      class="gallery__item" href=${original}>
+      <img class="gallery__image" 
+      src=${preview} 
+      alt=${description} /></a>`;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  animationSpeed: 250,
+  overlay: true,
+  close: false,
+  showCounter: false,
+  fadeSpeed: 200,
+});
