10572a315ae278bdca6bd0ea265f81eff4b0c2ed
anfernee84 <eduard.rogulin@outlook.com>
2022-09-01 09:24:32 +0300

    updates

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+var _ = require('lodash');
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+player.on('timeupdate', _.throttle(timeHandler, 1000));
+function timeHandler(data) {
+  console.log(data);
+}
+
+// player.on(‘timeupdate’, _.throttle(onUpdateTime, 1000));
