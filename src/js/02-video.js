0cc0ea5f59524968e8e9b230a62d05887ea08742
anfernee84 <eduard.rogulin@outlook.com>
2022-09-02 11:38:07 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,7 +10,7 @@ player.setCurrentTime(load('videoplayer-current-time') || 0);
 
 player.on('timeupdate', throttle(timeHandler, 1000));
 function timeHandler(data) {
-  // console.log(data);
+  console.log(data);
   save('videoplayer-current-time', data.seconds);
 }
 
